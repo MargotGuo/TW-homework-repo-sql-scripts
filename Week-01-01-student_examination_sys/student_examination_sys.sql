410fd4ca8e0cd4677e620bfc163c7289148a3f0e
MargotGuo <gyqgdn@126.com>
2020-03-19 13:26:59 +0800

    [Guo Yuanqi]{fix}: fix insert format

--- Week-01-01-student_examination_sys/student_examination_sys.sql
+++ Week-01-01-student_examination_sys/student_examination_sys.sql
@@ -7,8 +7,9 @@ CREATE TABLE student(
     age INT,
     sex VARCHAR(10)
 );
-INSERT INTO student(id, name, age, sex) VALUES ("001", "张三", 18, "男");
-INSERT INTO student(id, name, age, sex) VALUES ("002", "李四", 20, "女");
+INSERT INTO student(id, name, age, sex) VALUES
+	("001", "张三", 18, "男"),
+    ("002", "李四", 20, "女");
 
 CREATE TABLE subject(
 	id VARCHAR(10),
@@ -16,8 +17,9 @@ CREATE TABLE subject(
     teacher VARCHAR(32),
     description VARCHAR(32)
 );
-INSERT INTO subject(id, subject, teacher, description) VALUES ("1001", "语文", "王老师", "本次考试比较简单");
-INSERT INTO subject(id, subject, teacher, description) VALUES ("1002", "数学", "刘老师", "本次考试比较难");
+INSERT INTO subject(id, subject, teacher, description) VALUES 
+	("1001", "语文", "王老师", "本次考试比较简单"),
+    ("1002", "数学", "刘老师", "本次考试比较难");
 
 CREATE TABLE score(
 	id INT,
@@ -25,10 +27,11 @@ CREATE TABLE score(
     subject_id VARCHAR(10),
     score DOUBLE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-INSERT INTO score(id, student_id, subject_id, score) VALUES (1, "001", "1001", 80);
-INSERT INTO score(id, student_id, subject_id, score) VALUES (2, "002", "1002", 60);
-INSERT INTO score(id, student_id, subject_id, score) VALUES (3, "001", "1001", 70);
-INSERT INTO score(id, student_id, subject_id, score) VALUES (4, "002", "1002", 60.5);
+INSERT INTO score(id, student_id, subject_id, score) VALUES 
+	(1, "001", "1001", 80),
+    (2, "002", "1002", 60),
+	(3, "001", "1001", 70),
+    (4, "002", "1002", 60.5);
 
 SELECT id, student_id, subject_id, score FROM score;
 SELECT id, name, age, sex FROM student;
