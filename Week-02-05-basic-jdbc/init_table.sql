7a943216a51063e1be9533dfa98cdc9db7aee826
MargotGuo <gyqgdn@126.com>
2020-03-19 10:48:40 +0800

    [Guo Yuanqi]{feat}: refactor

--- Week-02-05-basic-jdbc/init_table.sql
+++ Week-02-05-basic-jdbc/init_table.sql
@@ -1,7 +1,9 @@
-CREATE DATABASE week_02_05_basic_jdbc DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS week_02_05_basic_jdbc
+DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
 USE week_02_05_basic_jdbc;
 
-CREATE TABLE student_info (
+CREATE TABLE IF NOT EXISTS student_info (
 	id VARCHAR(10),
 	name VARCHAR(20),
 	gender ENUM('男', '女'),
@@ -9,3 +11,7 @@ CREATE TABLE student_info (
     birthday DATE,
     classID VARCHAR(10)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+SELECT * FROM student_info;
+
+-- DROP TABLE IF EXISTS student_info; 
--- Week-02-05-basic-jdbc/src/main/java/com/thoughtworks/StudentRepository.java
+++ Week-02-05-basic-jdbc/src/main/java/com/thoughtworks/StudentRepository.java
@@ -2,6 +2,7 @@ package com.thoughtworks;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,8 +13,10 @@ import java.util.List;
 
 public class StudentRepository {
 
-  private static Connection connection = DatabaseUtil.getConnection();
-  private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+  private static final String URL = "jdbc:mysql://localhost:3306/week_02_05_basic_jdbc?useUnicode=true&characterEncoding=utf-8&serverTimezone=Hongkong";
+  private static final String USER = "root";
+  private static final String PASSWORD = "gyqpass";
+  private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
 
   public void save(List<Student> students) {
@@ -22,17 +25,16 @@ public class StudentRepository {
 
   public void save(Student student) {
     // TODO:
-    try {
-      String saveSql = "INSERT INTO student_info VALUES(?,?,?,?,?,?)";
-      PreparedStatement preparedStatement = connection.prepareStatement(saveSql);
 
+    String saveSql = "INSERT INTO student_info VALUES(?,?,?,?,?,?)";
+    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+         PreparedStatement preparedStatement = connection.prepareStatement(saveSql)) {
       preparedStatement.setString(1, student.getId());
       preparedStatement.setString(2, student.getName());
       preparedStatement.setString(3, student.getGender());
       preparedStatement.setInt(4, student.getAdmissionYear());
-      preparedStatement.setDate(5, java.sql.Date.valueOf(format.format(student.getBirthday())));
+      preparedStatement.setDate(5, java.sql.Date.valueOf(FORMAT.format(student.getBirthday())));
       preparedStatement.setString(6, student.getClassId());
-
       preparedStatement.execute();
     } catch (SQLException e) {
       e.printStackTrace();
@@ -42,10 +44,10 @@ public class StudentRepository {
   public List<Student> query() {
     // TODO:
     List<Student> studentList = new ArrayList<>();
-    try {
-      Statement statement = connection.createStatement();
-      ResultSet resultSet = statement.executeQuery("SELECT * FROM student_info");
-
+    String querySql = "SELECT * FROM student_info";
+    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+         Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery(querySql)) {
       while (resultSet.next()) {
         String id = resultSet.getString("id");
         String name = resultSet.getString("name");
@@ -54,23 +56,24 @@ public class StudentRepository {
         Date birthday = resultSet.getDate("birthday");
         String classID = resultSet.getString("classID");
 
-        Student student = new Student(id, name, gender, admissionYear, format.format(birthday), classID);
+        Student student = new Student(id, name, gender, admissionYear, FORMAT.format(birthday), classID);
         studentList.add(student);
       }
     } catch (SQLException e) {
       e.printStackTrace();
     }
+
     return studentList;
   }
 
   public List<Student> queryByClassId(String classId) {
     // TODO:
     List<Student> studentList = new ArrayList<>();
-    try {
-      Statement statement = connection.createStatement();
-      ResultSet resultSet = statement.executeQuery(
-          String.format("SELECT * FROM student_info WHERE classId = '%s' ORDER BY id DESC", classId));
+    String querySql = String.format("SELECT * FROM student_info WHERE classId = '%s' ORDER BY id DESC", classId);
 
+    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+         Statement statement = connection.createStatement();
+         ResultSet resultSet = statement.executeQuery(querySql)) {
       while (resultSet.next()) {
         String id = resultSet.getString("id");
         String name = resultSet.getString("name");
@@ -78,27 +81,25 @@ public class StudentRepository {
         int admissionYear = resultSet.getInt("admissionYear");
         Date birthday = resultSet.getDate("birthday");
         String classID = resultSet.getString("classID");
-
-        Student student = new Student(id, name, gender, admissionYear, format.format(birthday), classID);
+        Student student = new Student(id, name, gender, admissionYear, FORMAT.format(birthday), classID);
         studentList.add(student);
       }
     } catch (SQLException e) {
       e.printStackTrace();
     }
+
     return studentList;
   }
 
   public void update(String id, Student student) {
     // TODO:
-    try {
-      String updateSql = String.format(
-          "UPDATE student_info "
-              + "SET name = '%s', gender = '%s', admissionYear = %d, birthday = '%s', classId = '%s' "
-              + "WHERE id = '%s'",
-          student.getName(), student.getGender(), student.getAdmissionYear(),
-          format.format(student.getBirthday()), student.getClassId(), id);
-
-      Statement statement = connection.createStatement();
+    String updateSql = String.format("UPDATE student_info " +
+            "SET name = '%s', gender = '%s', admissionYear = %d, birthday = '%s', classId = '%s' " +
+            "WHERE id = '%s'",
+        student.getName(), student.getGender(), student.getAdmissionYear(),
+        FORMAT.format(student.getBirthday()), student.getClassId(), id);
+    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+         Statement statement = connection.createStatement()) {
       statement.executeUpdate(updateSql);
     } catch (SQLException e) {
       e.printStackTrace();
@@ -108,8 +109,8 @@ public class StudentRepository {
   public void delete(String id) {
     // TODO:
     String deleteSql = "DELETE from student_info WHERE id = ?";
-    try {
-      PreparedStatement preparedStatement = connection.prepareStatement(deleteSql);
+    try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+         PreparedStatement preparedStatement = connection.prepareStatement(deleteSql);) {
       preparedStatement.setString(1, id);
       preparedStatement.execute();
     } catch (SQLException e) {
